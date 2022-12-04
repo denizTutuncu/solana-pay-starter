d6ea6f817d5cec54739b7dee4349ea85a5f8a478
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-12-03 16:13:17 -0800

    Updates starter to AIC DAO Journal Store

--- components/Head.js
+++ components/Head.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import DAOImage from '../public/favicon.ico'
 
 export default function HeadComponent() {
   return (
@@ -7,23 +8,23 @@ export default function HeadComponent() {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="theme-color" content="#000000" />
 
-      <title>Solana Pay Store</title>
-      <meta name="title" content="Solana Pay Store" />
-      <meta name="description" content="Buy items on my store using Solana Pay!" />
+      <title>AIC DAO Journal Store</title>
+      <meta name="title" content="AIC DAO Journal Store" />
+      <meta name="description" content={DAOImage} />
 
       {/* Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://buildspace.so/" />
-      <meta property="og:title" content="Solana Pay Store" />
-      <meta property="og:description" content="Buy items on my store using Solana Pay!" />
-      <meta property="og:image" content="https://cdn.buildspace.so/courses/solana-pay/metadata.png" />
+      <meta property="og:url" content="https://aicities.vercel.app/" />
+      <meta property="og:title" content="AIC DAO Journal Store" />
+      <meta property="og:description" content="Buy AIC DAO journals on AIC DAO using Solana Pay!" />
+      <meta property="og:image" content={DAOImage} />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://buildspace.so/" />
-      <meta property="twitter:title" content="Solana Pay Store" />
-      <meta property="twitter:description" content="Buy items on my store using Solana Pay!" />
-      <meta property="twitter:image" content="https://cdn.buildspace.so/courses/solana-pay/metadata.png" />
+      <meta property="twitter:url" content="https://aicities.vercel.app/" />
+      <meta property="twitter:title" content="AIC DAO Journal Store" />
+      <meta property="twitter:description" content="Buy AIC DAO journals on AIC DAO using Solana Pay!" />
+      <meta property="twitter:image" content={DAOImage} />
     </Head>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,84 @@
 import React from "react";
 import HeadComponent from '../components/Head';
-
+// import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
+// import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import dynamic from "next/dynamic";
+import { useState, useEffect } from "react";
+import Product from "../components/Product"
 // Constants
-const TWITTER_HANDLE = "_buildspace";
+const TWITTER_HANDLE = "_aic";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
-  return (
-    <div className="App">
-      <HeadComponent/>
-      <div className="container">
-        <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
-        </header>
+  // Dynamic import `WalletMultiButton` to prevent hydration error
+  const WalletMultiButtonDynamic = dynamic(
+      async () =>
+        (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+      { ssr: false }
+    );
+    
+    // This will fetch the users' public key (wallet address) from any wallet we support
+    const { publicKey } = useWallet();
+    const [products, setProducts] = useState([]);
 
-        <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
-        </main>
-
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+    useEffect(() => {
+      if (publicKey) {
+        fetch(`/api/fetchProducts`)
+          .then(response => response.json())
+          .then(data => {
+            setProducts(data);
+            console.log("Products", data);
+          });
+      }
+    }, [publicKey]);
+  
+    const renderNotConnectedContainer = () => (
+      <div>
+        <div className="button-container">
+          <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
+        </div>    
+      </div>
+    );
+    
+    const renderItemBuyContainer = () => (
+      <div className="products-container">
+        {products.map((product) => (
+          < Product key={product.id} product={product} />
+        ))}
+      </div>
+    );
+  
+    return (
+      <div className="App">
+        <HeadComponent/>
+        <div className="container">
+          <header className="header-container">
+            <p className="header"> The AIC Times 🏙🌇🌆🌃 </p>
+            <p className="sub-text">Weekly City Journal by AIC DAO</p>
+            <p className="sub-text"> 🥐🥞🥯🍵☕️🧋🍔🌮🍕🍺🍸🪩</p>
+            <img src="https://media.giphy.com/media/1Rj8aaXgAvMoGN2bYu/giphy.gif" alt="emoji" />
+          </header>
+  
+          <main>
+            {/* We only render the connect button if public key doesn't exist */}
+            
+            {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+  
+          </main>
+  
+          <div className="footer-container">
+            <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+            <a
+              className="footer-text"
+              href={TWITTER_LINK}
+              target="_blank"
+              rel="noreferrer"
+            >{`built on @${TWITTER_HANDLE}`}</a>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default App;
+    );
+  };
+  
+  export default App;
