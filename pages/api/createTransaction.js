d5977ea7e95b85c4e40eb7df7ce930f5f4a5c354
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2022-12-07 08:24:13 -0800

    Adds create product & orderbook for tracking the buys

--- pages/api/createTransaction.js
+++ pages/api/createTransaction.js
@@ -9,16 +9,18 @@ import {
 } from "@solana/web3.js";
 import BigNumber from "bignumber.js";
 import products from "./products.json";
+import { createTransferCheckedInstruction, getAssociatedTokenAddress, getMint } from "@solana/spl-token";
 
+const usdcAddress = new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr");
 // Make sure you replace this with your wallet address! DAO Treasury
 const sellerAddress = 'GBByA19DBaJuSYUZU6fckCv6BmHxn8wR2HdBu2YNkB2B'
-const sellerPublicKey = new PublicKey(sellerAddress);
+const sellerUSDCAddress = '5BP8Pu21u1NxsJvG2rXrHUC9Gq5icgcgzgLFNLyGzQa3'
+const sellerPublicKey = new PublicKey(sellerUSDCAddress);
 
 const createTransaction = async (req, res) => {
   try {
     // Extract the transaction data from the request body
     const { buyer, orderID, itemID } = req.body;
-
     // If we don't have something we need, stop!
     if (!buyer) {
       return res.status(400).json({
@@ -34,6 +36,7 @@ const createTransaction = async (req, res) => {
 
     // Fetch item price from products.json using itemID
     const itemPrice = products.find((item) => item.id === itemID).price;
+    console.log("itemPrice", itemPrice);
 
     if (!itemPrice) {
       return res.status(404).json({
@@ -47,13 +50,9 @@ const createTransaction = async (req, res) => {
     const network = WalletAdapterNetwork.Devnet;
     const endpoint = clusterApiUrl(network);
     const connection = new Connection(endpoint);
-
     // A blockhash is sort of like an ID for a block. It lets you identify each block.
     const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash("finalized");
-    
-    // The first two things we need - a recent block ID 
-    // and the public key of the fee payer 
- 
+
     const tx = new Transaction({
         blockhash: blockhash,
         feePayer: buyerPublicKey,
@@ -88,6 +87,7 @@ const createTransaction = async (req, res) => {
     res.status(200).json({
       transaction: base64,
     });
+
   } catch (error) {
     console.error(error);
 
@@ -95,6 +95,98 @@ const createTransaction = async (req, res) => {
     return;
   }
 }
+
+const createUSDCTransaction = async (req, res) => {
+    try {
+      // Extract the transaction data from the request body
+      const { buyer, orderID, itemID } = req.body;
+      console.log("buyer", buyer);
+      console.log("orderID", orderID);
+      console.log("itemID", itemID);
+      console.log("orderID", orderID);
+     
+  
+      // If we don't have something we need, stop!
+      if (!buyer) {
+        return res.status(400).json({
+          message: "Missing buyer address",
+        });
+      }
+  
+      if (!orderID) {
+        return res.status(400).json({
+          message: "Missing order ID",
+        });
+      }
+  
+      // Fetch item price from products.json using itemID
+      const itemPrice = products.find((item) => item.id === itemID).price;
+      console.log("itemPrice", itemPrice);
+  
+      if (!itemPrice) {
+        return res.status(404).json({
+          message: "Item not found. please check item ID",
+        });
+      }
+      
+      // Convert our price to the correct format
+      const bigAmount = BigNumber(itemPrice);
+      const buyerPublicKey = new PublicKey(buyer);
+      const network = WalletAdapterNetwork.Devnet;
+      const endpoint = clusterApiUrl(network);
+      const connection = new Connection(endpoint);
+  
+      // A blockhash is sort of like an ID for a block. It lets you identify each block.
+      const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash("finalized");
+  
+    //   MARK:- USDC PAYMENT STARTS ***
+    //   This is new, we're getting the mint address of the token we want to transfer
+      const buyerUsdcAddress = await getAssociatedTokenAddress(usdcAddress, buyerPublicKey);
+      const shopUsdcAddress = await getAssociatedTokenAddress(usdcAddress, sellerPublicKey);
+      console.log("shopUsdcAddress", shopUsdcAddress.toString());
+      const usdcMint = await getMint(connection, usdcAddress);
+  
+      const tx = new Transaction({
+          blockhash: blockhash,
+          feePayer: buyerPublicKey,
+          lastValidBlockHeight: lastValidBlockHeight
+      });
+  
+      // Here we're creating a different type of transfer instruction
+      const usdcTransferInstruction = createTransferCheckedInstruction(
+          buyerUsdcAddress, 
+          usdcAddress,     // This is the address of the token we want to transfer
+          shopUsdcAddress, 
+          buyerPublicKey, 
+          bigAmount.toNumber() * 10 ** (await usdcMint).decimals, 
+          usdcMint.decimals // The token could have any number of decimals
+      );
+        // The rest remains the same :)
+      usdcTransferInstruction.keys.push({
+          pubkey: new PublicKey(orderID),
+          isSigner: false,
+          isWritable: false,
+      });
+  
+  
+      tx.add(usdcTransferInstruction);
+  
+      const usdcSerializedTransaction = tx.serialize({
+          requireAllSignatures: false,
+      });
+  
+      const base64 = usdcSerializedTransaction.toString("base64");
+  
+      res.status(200).json({
+          transaction: base64,
+        });
+    } catch (error) {
+      console.error(error);
+  
+      res.status(500).json({ error: "error creating tx" });
+      return;
+    }
+  }
 
 export default function handler(req, res) {
   if (req.method === "POST") {
--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,85 @@
 import React from "react";
 import HeadComponent from '../components/Head';
-// import { PublicKey } from '@solana/web3.js';
 import { useWallet } from '@solana/wallet-adapter-react';
-// import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import dynamic from "next/dynamic";
 import { useState, useEffect } from "react";
 import Product from "../components/Product"
+import CreateProduct from "../components/CreateProduct";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+
 // Constants
 const TWITTER_HANDLE = "_aic";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  // Dynamic import `WalletMultiButton` to prevent hydration error
-  const WalletMultiButtonDynamic = dynamic(
-      async () =>
-        (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
-      { ssr: false }
-    );
-    
-    // This will fetch the users' public key (wallet address) from any wallet we support
-    const { publicKey } = useWallet();
-    const [products, setProducts] = useState([]);
+  const { publicKey } = useWallet();
+  const isOwner = ( publicKey ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY : false );
+  const [creating, setCreating] = useState(false);
+  const [products, setProducts] = useState([]);
+  
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
 
-    useEffect(() => {
-      if (publicKey) {
-        fetch(`/api/fetchProducts`)
-          .then(response => response.json())
-          .then(data => {
-            setProducts(data);
-            console.log("Products", data);
-          });
-      }
-    }, [publicKey]);
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
   
-    const renderNotConnectedContainer = () => (
-      <div>
-        <div className="button-container">
-          <WalletMultiButtonDynamic className="cta-button connect-wallet-button" />
-        </div>    
-      </div>
-    );
-    
-    const renderItemBuyContainer = () => (
-      <div className="products-container">
-        {products.map((product) => (
-          < Product key={product.id} product={product} />
-        ))}
-      </div>
-    );
-  
-    return (
-      <div className="App">
-        <HeadComponent/>
-        <div className="container">
-          <header className="header-container">
-            <p className="header"> The AIC Times 🏙🌇🌆🌃 </p>
-            <p className="sub-text">Weekly City Journal by AIC DAO</p>
-            <p className="sub-text"> 🥐🥞🥯🍵☕️🧋🍔🌮🍕🍺🍸🪩</p>
-            <img src="https://media.giphy.com/media/1Rj8aaXgAvMoGN2bYu/giphy.gif" alt="emoji" />
-          </header>
-  
-          <main>
-            {/* We only render the connect button if public key doesn't exist */}
-            
-            {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
-  
-          </main>
-  
-          <div className="footer-container">
-            <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
-            <a
-              className="footer-text"
-              href={TWITTER_LINK}
-              target="_blank"
-              rel="noreferrer"
-            >{`built on @${TWITTER_HANDLE}`}</a>
-          </div>
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <HeadComponent/>
+      <div className="container">
+        <header className="header-container">
+          <p className="header">AIC DAO STORE</p>
+          <p className="sub-text">Weekly publication for places to see around the world by AI CITY DAO  🏙🌇🌆🌃 </p>
+          <p className="sub-text">🥐🥞🥯🍵☕️🧋🍔🌮🍕🍺🍸🪩</p>
+          <img width="750" height="400" src="https://media.giphy.com/media/1Rj8aaXgAvMoGN2bYu/giphy.gif" alt="emoji" />
+
+          {isOwner && (
+            <button className="create-product-button" onClick={() => setCreating(!creating)}>
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )}
+        </header>
+
+        <main>
+          {creating && <CreateProduct />}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
+        </main>
+
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
   
-  export default App;
+export default App;
